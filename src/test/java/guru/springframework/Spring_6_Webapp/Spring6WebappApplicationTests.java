78f27f0f6e11f5f5387597be1f8d28b3b5eefec3
Kopanye Ramokgopa <kopanye.ramokgopa1@vcontractor.co.za>
2026-06-08 09:10:27 +0200

    Initial commit

---
+++ src/test/java/guru/springframework/Spring_6_Webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.Spring_6_Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
